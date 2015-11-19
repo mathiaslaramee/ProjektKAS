ebec0d276626c5697c4f6049a80c068d8291afde
Mathias <Mathias@10.10.132.4>
2015-11-19 11:45:40 +0100

    Storage færdig

---
+++ KAS_Projekt/src/storage/Storage.java
@@ -0,0 +1,121 @@
+package storage;
+
+import java.util.ArrayList;
+
+import application.Booking;
+import application.Conference;
+import application.ExtraService;
+import application.Hotel;
+import application.Participant;
+import application.Trip;
+
+public class Storage {
+	
+	private static ArrayList<Participant> participants = new ArrayList<>();
+	private static ArrayList<Hotel> hotels = new ArrayList<>();
+	private static ArrayList<Conference> conferences = new ArrayList<>();
+	private static ArrayList<Booking> bookings = new ArrayList<>();
+	private static ArrayList<ExtraService> services = new ArrayList<>();
+	private static ArrayList<Trip> trips = new ArrayList<>();
+	
+	
+	public static ArrayList<Trip> getTrips()
+	{
+		return new ArrayList<Trip>(trips);
+	}
+	
+	public static void removeTrip(Trip trip)
+	{
+		trips.remove(trip);
+	}
+	
+	public static void addTrip(Trip trip)
+	{
+		trips.add(trip);
+	}
+	
+	//-------------------------------------------------------------------------------------------------------------------------------//
+	
+	public static ArrayList<ExtraService> getServices()
+	{
+		return new ArrayList<ExtraService>(services);
+	}
+	
+	public static void removeService(ExtraService ExtraService)
+	{
+		services.remove(ExtraService);
+	}
+	
+	public static void addService(ExtraService ExtraService)
+	{
+		services.add(ExtraService);
+	}
+	
+	//-------------------------------------------------------------------------------------------------------------------------------//
+	
+	public static ArrayList<Booking> getBookings()
+	{
+		return new ArrayList<Booking>(bookings);
+	}
+	
+	public static void removeBooking(Booking booking)
+	{
+		bookings.remove(booking);
+	}
+	
+	public static void addBooking(Booking booking)
+	{
+		bookings.add(booking);
+	}
+	
+	//-------------------------------------------------------------------------------------------------------------------------------//
+	
+	public static ArrayList<Participant> getParticipants()
+	{
+		return new ArrayList<Participant>(participants);
+	}
+	
+	public static void removeParticipant(Participant participant)
+	{
+		participants.remove(participant);
+	}
+	
+	public static void addParticipant(Participant participant)
+	{
+		participants.add(participant);
+	}
+	
+	//-------------------------------------------------------------------------------------------------------------------------------//
+	
+	public static ArrayList<Hotel> getHotels()
+	{
+		return new ArrayList<Hotel>(hotels);
+	}
+
+	public static void removeHotel(Hotel hotel)
+	{
+		hotels.remove(hotel);
+	}
+	
+	public static void addHotel(Hotel hotel)
+	{
+		hotels.add(hotel);
+	}
+	
+	//-------------------------------------------------------------------------------------------------------------------------------//
+	
+	public static ArrayList<Conference> getConference()
+	{
+		return new ArrayList<Conference>(conferences);
+	}
+	
+	public static void removeConference(Conference conference)
+	{
+		conferences.remove(conference);
+	}
+	
+	public static void addConference(Conference conference)
+	{
+		conferences.add(conference);
+	}
+}
