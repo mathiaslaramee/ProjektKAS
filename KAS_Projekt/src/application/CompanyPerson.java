2c642c68f8b7264f0fdb123a254a63023c2fedfe
Mathias <Mathias@10.10.132.4>
2015-11-19 09:42:50 +0100

    Opstart2

---
+++ KAS_Projekt/src/application/CompanyPerson.java
@@ -0,0 +1,14 @@
+package application;
+
+public class CompanyPerson extends Participant {
+
+	public CompanyPerson(String name, int age, String country, String city, int phonenumber) {
+		super(name, age, country, city, phonenumber);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	
+}
