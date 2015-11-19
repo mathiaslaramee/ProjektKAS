5979ac8dac6756e8b756b4485f518cc49408963a
Mathias <Mathias@10.10.132.4>
2015-11-19 10:21:49 +0100

    Conference oprettet

---
+++ KAS_Projekt/src/application/Conference.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Conference {
+	
+	private String conferenceName;
+	private String adress;
+	private double conferenceCost;
+	private ArrayList<Hotel> hotels = new ArrayList<>();
+	private ArrayList<Trip> trips = new ArrayList<>();
+	private ArrayList<Booking> bookings = new ArrayList<>();
+	
+
+}
